b9e264fa14a429be8dc38f67d5139ee703cf6c6c
Jose Ramón Cajide <joseramoncajide@users.noreply.github.com>
2019-09-21 09:03:22 +0200

    fix

--- 06-Google-Search-Console-API/06-gsc-api.R
+++ 06-Google-Search-Console-API/06-gsc-api.R
@@ -9,7 +9,7 @@ new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"
 if(length(new.packages)) install.packages(new.packages)
 lapply(list.of.packages, require, character.only = TRUE)
 
-scr_auth(new_user = TRUE)
+scr_auth()
 
 list_websites()
 
